91d608dd161cdfa15e60561e46586fcedf8fa77f
Maria-Moiseeva <113455443+Maria-Moiseeva@users.noreply.github.com>
2023-06-27 12:16:14 +0300

    Add files via upload
    task 2

---
+++ main.py
@@ -0,0 +1,10 @@
+class BankAccount(object):
+    """docstring"""
+def __init__(self):
+        """Constructor"""
+        pass
+def __init__(self, account_number, balance, owner_name):
+        """Constructor"""
+        self.account_number = account_number
+        self.balance = balance
+        self.owner_name = owner_name
